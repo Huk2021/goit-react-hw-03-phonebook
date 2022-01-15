c667cde67d65c5eb0ad1a6eeff1d85010fb66831
Viktoriia Huk <relax16011991@gmail.com>
2022-01-15 21:20:23 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,16 +9,11 @@ import contacts from "./contacts.json";
 class App extends Component {
   state = {
     contacts: contacts,
-    // { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    // { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    // { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    // { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-
     filter: "",
   };
 
   componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
+    const contacts = localStorage.getItem("contacts");
     const parsedContacts = JSON.parse(contacts);
 
     if (parsedContacts) {
@@ -28,7 +23,7 @@ class App extends Component {
 
   componentDidUpdate(prevProps, prevState) {
     if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
     }
   }
 
@@ -60,7 +55,6 @@ class App extends Component {
 
   changeFilter = (e) => {
     this.setState({ filter: e.target.value });
-    // console.log(e.target.value)
   };
 
   getFilterContacts = () => {
